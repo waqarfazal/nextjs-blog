0fe54d871bece8de683e55d8308d8ece294eaef3
Waqar Fazal <34754447+waqarfazal@users.noreply.github.com>
2022-04-05 20:25:07 +0500

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/Link';
+import Link from 'next/link'
 import Date from '../components/date';
 export async function getStaticProps(){
   const allPostsData = getSortedPostsData();
